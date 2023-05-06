711b04f63531b425af5f80e33d7281f12166debb
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-05 22:04:40 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+int faces, rolagem;
+
+Console.Clear();
+
+
+
+
+
+Console.WriteLine($"# Rolagem de dados #");
+Console.WriteLine();
+
+Console.Write("Quantas faces?.... ");
+faces = Convert.ToInt32(Console.ReadLine());
+
+
+rolagem = RandomNumberGenerator.GetInt32(1, faces + 1);
+Console.Write("Rolagem........... ");
+Console.Write(rolagem);
